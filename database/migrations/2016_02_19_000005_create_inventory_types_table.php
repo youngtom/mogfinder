5034cbf6978091b7e17c8a573dbe20c2f8c275cf
Tom Young <tom@freshiscool.com>
2016-03-15 21:21:38 -0700

    Removing unnecessary migrations

--- database/migrations/2016_02_19_000005_create_inventory_types_table.php
+++ database/migrations/2016_02_19_000005_create_inventory_types_table.php
@@ -14,7 +14,7 @@ class CreateInventoryTypesTable extends Migration
     {
         Schema::create('inventory_types', function (Blueprint $table) {
             $table->increments('id');
-            
+            $table->string('name')->after('id'); 
             $table->timestamps();
         });
     }
