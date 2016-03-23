159cdb3e3e73e1fc1bd44097ed5927348d0cdaa4
Tom Young <tom@freshiscool.com>
2016-03-23 12:59:32 -0700

    Moving bnet API key to env settings

--- config/settings.php
+++ config/settings.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-	'bnet_api_key' => 'u5djg84x69nnbhpqwh57gxsyq5d3xk5k',
+	'bnet_api_key' => env('BNET_API_KEY', true),
 	'bnet_api_locale' => 'en_US',
 	'bnet_api_base_url' => 'https://{$region}.api.battle.net/wow',
 	'bnet_api_cache_expiration' => (60*60*24*90), // 90 days
