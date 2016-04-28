a7c88c2aca9dee0dd48ea148f9f3acf39c71ab7d
Tom Young <tom@freshiscool.com>
2016-04-28 02:22:58 -0700

    Achievement source import

--- app/Console/Commands/ItemSourceDataImportHelper.php
+++ app/Console/Commands/ItemSourceDataImportHelper.php
@@ -349,12 +349,11 @@ class ItemSourceDataImportHelper extends Command
 							foreach ($item->itemSources as $source) {
 								if ($source->item_source_type_id != 13 || !in_array($source->bnet_source_id, $sourceArr)) {
 									fwrite($fp, 'Deleting source - itemID: ' . $item->id . ' bnetID: ' . $source->bnet_source_id . ' typeID: ' . $source->item_source_type_id . "\n");
-									//$source->delete();
+									$source->delete();
 								}
 							}
 							
 							foreach ($sourceArr as $sourceBnetID) {
-								break;
 								$source = ItemSource::where('item_id', '=', $item->id)->where('bnet_source_id', '=', $sourceBnetID)->where('item_source_type_id', '=', 13)->first();
 								
 								if (!$source) {
