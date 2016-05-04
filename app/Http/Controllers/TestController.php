de26e41b207bdac8554e378aa75dd212b9ac6001
Tom Young <tom@freshiscool.com>
2016-05-04 04:59:43 -0700

    World drop info test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -24,6 +24,150 @@ use App\Boss;
 class TestController extends Controller
 {
 	public function index() {
+		//$sources = ItemSource::where('item_source_type_id', '=', 3)->get();
+		
+		$file = file(storage_path() . '/logs/PROD.laravel6.log');
+		
+		$_out = [
+			'boss' => [],
+			'zone' => [],
+			'no-location' => [],
+			'no-location-object' => [],
+			'multiple-locs' => [],
+			'no-vendor-loc' => [],
+			'multiple-spells' => []
+		];
+		
+		foreach ($file as $line) {
+			
+			if (stristr($line, 'production.INFO')) {
+				list($timestamp, $typeStr, $itemStr, $bnetID) = explode(': ', trim($line));
+				$bnetID = trim(str_replace(')', '', $bnetID));
+				if (preg_match_all('/Boss \((\d+)\) not found for item/', $typeStr, $matches)) {
+					$bossID = $matches[1][0];
+					
+					if (!array_key_exists($bossID, $_out['boss'])) {
+						$_out['boss'][$bossID] = [];
+					}
+					
+					if (!in_array($bnetID, $_out['boss'][$bossID])) {
+						$_out['boss'][$bossID][] = $bnetID;
+					}
+				} elseif (preg_match_all('/Zone \((\d+)\) not found for item/', $typeStr, $matches)) {
+					$zoneID = $matches[1][0];
+					
+					if (!array_key_exists($zoneID, $_out['zone'])) {
+						$_out['zone'][$zoneID] = [];
+					}
+					
+					if (!in_array($bnetID, $_out['zone'][$zoneID])) {
+						$_out['zone'][$zoneID][] = $bnetID;
+					}
+				} elseif ($typeStr == 'Location info not available for item drop') {
+					if (!in_array($bnetID, $_out['no-location'])) {
+						$_out['no-location'][] = $bnetID;
+					}
+				} elseif ($typeStr == 'Location info not available for item in object') {
+					if (!in_array($bnetID, $_out['no-location-object'])) {
+						$_out['no-location-object'][] = $bnetID;
+					}
+				} elseif (preg_match_all('/Multiple locations for NPC \((\d+)\) for item/', $typeStr, $matches)) {
+					$bossID = $matches[1][0];
+					
+					if (!array_key_exists($bossID, $_out['multiple-locs'])) {
+						$_out['multiple-locs'][$bossID] = [];
+					}
+					
+					if (!in_array($bnetID, $_out['multiple-locs'][$bossID])) {
+						$_out['multiple-locs'][$bossID][] = $bnetID;
+					}
+				} elseif (preg_match_all('/NPC \((\d+)\) location not found for item/', $typeStr, $matches)) {
+					$bossID = $matches[1][0];
+					
+					if (!array_key_exists($bossID, $_out['no-vendor-loc'])) {
+						$_out['no-vendor-loc'][$bossID] = [];
+					}
+					
+					if (!in_array($bnetID, $_out['no-vendor-loc'][$bossID])) {
+						$_out['no-vendor-loc'][$bossID][] = $bnetID;
+					}
+				} elseif ($typeStr == 'Item created by multiple spells') {
+					if (!in_array($bnetID, $_out['mutliple-spells'])) {
+						$_out['no-location'][] = $bnetID;
+					}
+				}
+			}
+		}
+		//dd($_out);
+		
+		$out = [];
+		
+		foreach ($_out as $label => $dataArr) {
+			if (count($dataArr)) {
+				if ($label == 'boss') {
+					$out[] = '<h4>NPCs (instance but not boss) - added</h4>';
+					foreach ($dataArr as $bossID => $itemIDArr) {
+						$str = '<a href="http://www.wowhead.com/npc=' . $bossID . '">NPC ' . $bossID . '</a> - Items: ';
+						foreach ($itemIDArr as $itemID) {
+							$str .= '<a href="http://www.wowhead.com/item=' . $itemID . '">' . $itemID . '</a> ';
+						}
+						$str .= '<br>';
+						$out[] = $str;
+					}
+				} elseif ($label == 'zone') {
+					$out[] = '<h4>Zones</h4>';
+					foreach ($dataArr as $zoneID => $itemIDArr) {
+						$str = '<a href="http://www.wowhead.com/zone=' . $zoneID . '">Zone ' . $zoneID . '</a> - Items: ';
+						foreach ($itemIDArr as $itemID) {
+							$str .= '<a href="http://www.wowhead.com/item=' . $itemID . '">' . $itemID . '</a> ';
+						}
+						$str .= '<br>';
+						$out[] = $str;
+					}
+				} elseif ($label == 'no-location') {
+					$out[] = '<h4>No location (single NPC drop) - not added</h4>';
+					foreach ($dataArr as $itemID) {
+						$out[] = '<a href="http://www.wowhead.com/item=' . $itemID . '">' . $itemID . '</a><br>';
+					}
+				} elseif ($label == 'no-location-object') {
+					$out[] = '<h4>No location (object) - not added</h4>';
+					foreach ($dataArr as $itemID) {
+						$out[] = '<a href="http://www.wowhead.com/item=' . $itemID . '">' . $itemID . '</a><br>';
+					}
+				} elseif ($label == 'multiple-locs') {
+					$out[] = '<h4>NPCs in multiple locations (add sources manually) - not added</h4>';
+					foreach ($dataArr as $bossID => $itemIDArr) {
+						$str = '<a href="http://www.wowhead.com/npc=' . $bossID . '">NPC ' . $bossID . '</a> - Items: ';
+						foreach ($itemIDArr as $itemID) {
+							$str .= '<a href="http://www.wowhead.com/item=' . $itemID . '">' . $itemID . '</a> ';
+						}
+						$str .= '<br>';
+						$out[] = $str;
+					}
+				} elseif ($label == 'no-vendor-loc') {
+					$out[] = '<h4>No location for vendor NPCs (might be legacy) - added</h4>';
+					foreach ($dataArr as $bossID => $itemIDArr) {
+						$str = '<a href="http://www.wowhead.com/npc=' . $bossID . '">NPC ' . $bossID . '</a> - Items: ';
+						foreach ($itemIDArr as $itemID) {
+							$str .= '<a href="http://www.wowhead.com/item=' . $itemID . '">' . $itemID . '</a> ';
+						}
+						$str .= '<br>';
+						$out[] = $str;
+					}
+				} elseif ($label == 'multiple-spells') {
+					$out[] = '<h4>Multiple Spells - not added</h4>';
+					foreach ($dataArr as $itemID) {
+						$out[] = '<a href="http://www.wowhead.com/item=' . $itemID . '">' . $itemID . '</a><br>';
+					}
+				}
+			}
+			
+			$out[] = '<br>';
+		}
+	   
+	   return view('test')->with('out', $out)->with('newline', "\n");
+		
+		/*
 		$sources = ItemSource::where('item_source_type_id', '=', 6)->orderBy('bnet_source_id', 'ASC')->get()->groupBy('bnet_source_id');
 	    $out = [];
 	    
@@ -46,8 +190,6 @@ class TestController extends Controller
 			}
 	    }
 	    
-	    return view('test')->with('out', $out)->with('newline', "\n");
-		/*
 		$bosses = [
 			'1048|71543' => '110784,110785,112382,112383,112416,112417,112418,112419,112420,112421,112422,112423,112424,112425,112425,112425,112428,112429,112445,112447,112448',
 			'1057|71734' => '112702,112949,112950,112951,112952,112953'
@@ -76,6 +218,23 @@ class TestController extends Controller
 		}
 		dd($bosses);
 		*/
+	}
+	
+	public function worldDropInfo() {
+		$items = Item::whereIn('id', function ($query) {
+		    $query->select('item_id')->from('item_sources')->where('item_source_type_id', '=', 3);
+	    })->where('transmoggable', '=', 1)->orderBy('bnet_id', 'ASC')->get();
+		
+		foreach ($items as $item) {
+			if ($items->itemSources->count() > 1) {
+				$out[] = '<a href="http://www.wowhead.com/item=' . $item->bnet_id . '" class="q' . $item->quality . '" rel="' . $item->getWowheadMarkup() . '">[' . $item->name . ']</a>';
+				foreach ($items->itemSources as $source) {
+					$out[] = '-- <a href="' . $source->getWowheadLink($item) . '">' . $source->getSourceText() . '</a>';
+				}
+			}
+		}
+		
+		return view('test')->with('out', $out)->with('newline', "<br>");
 	}
 	
     public function checkDeletedSources($id) {
