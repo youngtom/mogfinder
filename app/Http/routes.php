70c4a0c7d6adb895daa4fc5b8901b218e9a9e663
Tom Young <tom@freshiscool.com>
2016-05-05 19:08:04 -0700

    Updating boss route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -62,7 +62,7 @@ Route::group(['middleware' => 'web'], function () {
     //wardrobe routes
     Route::get('/wardrobe', 'ItemsController@index');
     Route::get('/wardrobe/zone/{zoneURL}', 'ItemsController@showZoneDisplays');
-    Route::get('/wardrobe/zone/{zoneURL}/{bossURL}', 'ItemsController@showBossDisplays');
+    Route::get('/wardrobe/boss/{zoneURL}/{bossURL}', 'ItemsController@showBossDisplays');
     Route::get('/wardrobe/auctions', 'ItemsController@showAuctions');
     Route::get('/wardrobe/duplicates', 'ItemsController@duplicates');
     Route::get('/wardrobe/{characterURL}', 'ItemsController@index');
