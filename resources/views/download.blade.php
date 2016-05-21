ec591badef6f8714d244afdfa45e6ab67dd62806
Tom Young <tom@freshiscool.com>
2016-05-21 01:15:07 -0700

    Mod v2.0.5

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -5,7 +5,7 @@
 	<div class="col-md-10 col-md-offset-1">
 	    <div class="row">
 		    <div class="jumbotron">
-			    <h1>ItemCollector v2.0.4</h1>
+			    <h1>ItemCollector v2.0.5</h1>
 			    <p>This addon is used to scan items obtained on your account. To use it, just log in to each character you wish to scan, open your bank and void storage (and open the heirloom tab on one character). Type /mcc to show a summary of what has been scanned.</p>
 			    <p>
 				    <a class="btn btn-primary btn-lg" href="{{ asset('/files/ItemCollector.zip') }}" role="button">Download v2.0.4</a>
