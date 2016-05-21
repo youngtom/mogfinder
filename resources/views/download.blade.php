432f3bb0d20d8bb14b6d7c87989b21cef9ec4680
Tom Young <tom@freshiscool.com>
2016-05-21 01:15:47 -0700

    Really need to move the versioning to the db...

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -8,7 +8,7 @@
 			    <h1>ItemCollector v2.0.5</h1>
 			    <p>This addon is used to scan items obtained on your account. To use it, just log in to each character you wish to scan, open your bank and void storage (and open the heirloom tab on one character). Type /mcc to show a summary of what has been scanned.</p>
 			    <p>
-				    <a class="btn btn-primary btn-lg" href="{{ asset('/files/ItemCollector.zip') }}" role="button">Download v2.0.4</a>
+				    <a class="btn btn-primary btn-lg" href="{{ asset('/files/ItemCollector.zip') }}" role="button">Download v2.0.5</a>
 				    <a class="btn btn-info btn-lg" href="http://mods.curse.com/addons/wow/itemcollector" target="_blank" role="button">Download on Curse.com</a>
 			    </p>
 		    </div>
