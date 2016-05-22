2bc3d7babafa6f9df7ccb41df533a2c84ff54507
Tom Young <tom@freshiscool.com>
2016-05-22 00:35:32 -0700

    Removing progress bar fanciness until stable

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -1,50 +1,3 @@
-(function($) {
-    $.fn.countTo = function(options) {
-        // merge the default plugin settings with the custom options
-        options = $.extend({}, $.fn.countTo.defaults, options || {});
-
-        // how many times to update the value, and how much to increment the value on each update
-        var loops = Math.ceil(options.speed / options.refreshInterval),
-            increment = (options.to - options.from) / loops;
-
-        return $(this).each(function() {
-            var _this = this,
-                loopCount = 0,
-                value = options.from,
-                interval = setInterval(updateTimer, options.refreshInterval);
-
-            function updateTimer() {
-                value += increment;
-                loopCount++;
-                $(_this).html(value.toFixed(options.decimals));
-
-                if (typeof(options.onUpdate) == 'function') {
-                    options.onUpdate.call(_this, value);
-                }
-
-                if (loopCount >= loops) {
-                    clearInterval(interval);
-                    value = options.to;
-
-                    if (typeof(options.onComplete) == 'function') {
-                        options.onComplete.call(_this, value);
-                    }
-                }
-            }
-        });
-    };
-
-    $.fn.countTo.defaults = {
-        from: 0,  // the number the element should start at
-        to: 100,  // the number the element should end at
-        speed: 1000,  // how long it should take to count between the target numbers
-        refreshInterval: 100,  // how often the element should be updated
-        decimals: 0,  // the number of decimal places to show
-        onUpdate: null,  // callback method for every time the element is updated,
-        onComplete: null,  // callback method for when the element finishes updating
-    };
-})(jQuery);
-
 $(function () {	
     $('#luaupload').fileupload({
         dataType: 'json',
@@ -114,7 +67,6 @@ function updateProgressbar(current, total, $button) {
 	
 	var now = parseInt($bar.attr('aria-valuenow'));
 	$bar.attr('aria-valuemax', total);
-	var currentPct = $bar.attr('data-current-pct') ? parseInt($bar.attr('data-current-pct')) : 0;
 	
 	if (current > now) {
 		$bar.attr('aria-valuenow', current);
@@ -123,16 +75,7 @@ function updateProgressbar(current, total, $button) {
 			var percent = Math.min(100, Math.round((current / total) * 100));
 			$bar.attr('data-target-percent', percent);
 			$bar.width(percent + '%');
-			
-			$('.progress-label span', $bar).countTo({
-	            from: currentPct,
-	            to: Math.round(percent),
-	            speed: 1000,
-	            refreshInterval: 50,
-	            onComplete: function(value) {
-	                $bar.attr('data-current-pct', Math.round(percent));
-	            }
-	        });
+			$('.progress-label span', $bar).html(Math.round(percent));
 			
 			if (current >= total) {
 				var $button = $('#data-upload-button');
