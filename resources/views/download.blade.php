35360e6e77417b24cf2dacdba4aba9277acb5abf
Tom Young <tom@freshiscool.com>
2016-05-23 02:30:04 -0700

    Layout update

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -6,7 +6,9 @@
 	    <div class="row">
 		    <div class="jumbotron">
 			    <h1>ItemCollector v<?=$currentModVersion?></h1>
-			    <p>This addon is used to scan items obtained on your account. To use it, just log in to each character you wish to scan, open your bank and void storage (and open the heirloom tab on one character). Type /mcc to show a summary of what has been scanned. To scan the currently open guild bank tab, type /mcc gb (/mcc gbclear will clear all of your current guild's bank data).</p>
+			    <p>This addon is used to scan items obtained on your account. To use it, just log in to each character you wish to scan, open your bank and void storage (and open the heirloom tab on one character).</p>
+			    <p>Type /mcc to show a summary of what has been scanned.</p>
+			    <p>To scan the currently open guild bank tab, type /mcc gb (/mcc gbclear will clear all of your current guild's bank data).</p>
 			    <p>
 				    <a class="btn btn-primary btn-lg" href="{{ asset('/files/ItemCollector.zip') }}" role="button">Download v<?=$currentModVersion?></a>
 				    <a class="btn btn-info btn-lg" href="http://mods.curse.com/addons/wow/itemcollector" target="_blank" role="button">Download on Curse.com</a>
