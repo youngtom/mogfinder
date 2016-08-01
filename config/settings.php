58d80910f2b234a07782fd037705ac069b16990b
Tom Young <tom@freshiscool.com>
2016-08-01 14:34:28 -0700

    Max bnet id

--- config/settings.php
+++ config/settings.php
@@ -5,7 +5,7 @@ return array(
 	'bnet_api_locale' => 'en_US',
 	'bnet_api_base_url_default' => 'https://us.api.battle.net/wow',
 	'bnet_api_cache_expiration' => (60*60*24*90), // 90 days
-	'bnet_max_item_id' => 134000,
+	'bnet_max_item_id' => 142021,
 	'bnet_item_renders_base_url' => 'http://media.blizzard.com/wow/renders/items/',
 	'upload_file_dir' => 'app/uploads',
 	'download_file_dir' => 'app/downloads',
